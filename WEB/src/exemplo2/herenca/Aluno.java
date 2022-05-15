1ec01eaa4bbb273d11c07ae0a0e27f8c4c0a8d13
FernandoFrancaDev <104387118+FernandoFrancaDev@users.noreply.github.com>
2022-05-15 18:45:22 -0300

    Commit inicial de array

---
+++ WEB/src/exemplo2/herenca/Aluno.java
@@ -0,0 +1,13 @@
+package exemplo2.herenca;
+
+public class Aluno extends Pessoa {
+	private String cursos;
+	private String notas;
+
+	public String getCursos() {
+		return cursos;
+	}
+	public void setCursos(String cursos) {
+		this.cursos = cursos;
+	}
+}
